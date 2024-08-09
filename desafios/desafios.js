751ae3bef61e0ebccc91682d7ac87054219133c2
Hellen Moura <hellen.aparecidams@outlook.com>
2024-08-09 16:08:41 -0300

    exercicio inicial

---
+++ desafios/desafios.js
@@ -0,0 +1,22 @@
+// cliente = {
+//     nome: 'Mariana', 
+//     idade: 35,
+//     genero:'Feminino'
+//  }
+
+// //  cliente['estado'] = 'Pernambuco'
+//  cliente.estado = 'Pernambuco'
+
+// console.log('Olá, meu nome é ' + cliente.name + 'e moro no estado de ' + cliente.estado + '.')
+
+//let é uma variável que pode ser reatribuída e redeclarada
+//var vaza escopo de bloco 
+//const é uma variável que não pode ser reatribuída, mas pode ser redeclarada
+
+// EXEMPLO
+
+// function imprimeIdade() {
+//     for (let idade = 21; idade <= 100; idade++) {
+//         console.log('Minha idade é ' + idade)
+//     }
+// }
---
+++ main.js
@@ -0,0 +1,6 @@
+// let é uma variável que pode ser reatribuída e redeclarada
+// const é uma variável que não pode ser reatribuída, mas pode ser redeclarada
+// var é uma variável que pode ser reatribuída e redeclarada (não use)
+//variável nomeDoObjeto = { chave: valor}
+
+let listaDeCompras = []
