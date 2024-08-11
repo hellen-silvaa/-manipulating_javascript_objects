0f84ef9190e81da3abb269ce97505b3e05264536
Hellen Moura <hellen.aparecidams@outlook.com>
2024-08-11 20:14:27 -0300

    ajustando

--- main.js
+++ main.js
@@ -4,3 +4,13 @@
 //variável nomeDoObjeto = { chave: valor}
 
 let listaDeCompras = []
+
+const from = document.getElementById("form-itens")
+const input = document.getElementById("receber-item")
+
+// Criando interatividade na página a partir do click no botão
+//eventlistener é um método que é chamado quando um evento é disparado
+
+form.addEventListener("subimit", function (evento){
+    evento.preventDefault()
+})
